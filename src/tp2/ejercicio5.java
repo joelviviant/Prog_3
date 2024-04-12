bd9a32a8f22c23eb4e7418859961871f1c90c73d
joelviviant <joelviviant31@gmail.com>
2024-04-12 16:24:53 -0300

    ejercicio5

--- src/tp2/ejercicio5.java
+++ src/tp2/ejercicio5.java
@@ -1,4 +1,14 @@
 package tp2;
 
 public class ejercicio5 {
+
+    public static boolean elemetosIguales(int[] arr, int index) {
+        if (index == arr.length) {
+            return false;
+        }
+        if (arr[index] == index) {
+            return true;
+        }
+        return elemetosIguales(arr, index + 1);
+    }
 }

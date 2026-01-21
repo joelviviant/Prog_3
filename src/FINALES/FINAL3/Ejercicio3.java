ffeea5ce282623f2ec4b340b6046802d02477251
Joel Viviant <joelviviant31@gmail.com>
2026-01-21 15:41:56 -0300

    final 3 terminado

--- src/FINALES/FINAL3/Ejercicio3.java
+++ src/FINALES/FINAL3/Ejercicio3.java
@@ -1,5 +1,62 @@
 package FINALES.FINAL3;
 
 public class Ejercicio3 {
-    //Hashing lineal
+    //Hashing lineal  h(x)= x mod M
+    // Pd = 1,7
+
+    //0->27->12
+    //1->4->16->91
+    //2->
+
+    //m=3, f=0
+
+    //INGRESAR 8
+    //8 mod 3 = 2
+
+    //0->27->12
+    //1->4->16->91
+    //2->8
+
+    //pdcarga= 6/3 = 2
+
+    //se reordena el buffer 0 ya que ahi esta la frontera=0
+
+    //27 mod 6 = 2
+    //12 mod 6 =
+
+    //0->12
+    //1->4->16->91
+    //2->8
+    //3->27
+
+    //p de carga 1,5
+    //f =1
+
+
+    //INGRESAR EL 6
+
+    //6 mod 3= 0
+
+    //0->12->6
+    //1->4->16->91
+    //2->8
+    //3->27
+
+    //p de carga 1,75
+    //REORDENAMOS EL BUCKET 1 F=1
+
+    //4 mod 6 = 4
+    //16 mod 6 = 4
+    //91 mod 6 =1
+
+    //0->12->6
+    //1->91
+    //2->8
+    //3->27
+    //4->4->16
+
+    //p de carga =1,4 , f=2
+
+
+
 }
---
+++ src/FINALES/FINAL3/Ejercicio4.java
@@ -0,0 +1,49 @@
+package FINALES.FINAL3;
+
+
+public class Ejercicio4 {
+
+    public static void mergeSort(int[] arr) {
+        if (arr.length <= 1) return;
+
+        int medio = arr.length / 2;
+
+        int[] izquierda = new int[medio];
+        int[] derecha = new int[arr.length - medio];
+
+        // copiar elementos
+        for (int i = 0; i < medio; i++) {
+            izquierda[i] = arr[i];
+        }
+        for (int i = medio; i < arr.length; i++) {
+            derecha[i - medio] = arr[i];
+        }
+        // llamadas recursivas
+        mergeSort(izquierda);
+        mergeSort(derecha);
+        // fusionar
+        merge(arr, izquierda, derecha);
+    }
+
+    private static void merge(int[] arr, int[] izq, int[] der) {
+        int i = 0, j = 0, k = 0;
+
+        // comparar de MAYOR a MENOR
+        while (i < izq.length && j < der.length) {
+            if (izq[i] >= der[j]) {
+                arr[k++] = izq[i++];
+            } else {
+                arr[k++] = der[j++];
+            }
+        }
+
+        // copiar sobrantes
+        while (i < izq.length) {
+            arr[k++] = izq[i++];
+        }
+
+        while (j < der.length) {
+            arr[k++] = der[j++];
+        }
+    }
+}

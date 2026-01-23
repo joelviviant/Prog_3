9211d9ecc1b410e3ae06f98fb8411332466d7396
Joel Viviant <joelviviant31@gmail.com>
2026-01-23 08:23:54 -0300

    EJERCICIO 1 y 2 terminado

---
+++ src/FINALES/FINAL1/Ejercicio2.java
@@ -0,0 +1,41 @@
+package FINALES.FINAL1;
+
+public class Ejercicio2 {
+
+    //Arrancando desde B
+    //B->A = 6
+    //B->D = 10
+
+    //PADRE DE A Y D = B
+
+    //TOMAMOS A
+    //A->D =6 =12 X
+    //A->B = 6=12 X
+    //A->E = 2= 8 OK
+
+    //PADRE DE E= A
+
+    //TOMAMOS D
+
+    //D-> C = 6
+
+    //PADRE DE C = D
+
+    //TOMAMOS E
+
+    //E->D = 1 OK
+
+    //PADRE DE D = E
+
+//    B → B = 0
+//    B → A = 6
+//    B → E = 8
+//    B → D = 9
+//    B → C = 16
+//    B → F = ∞
+
+
+    //DKJISTRA SIRVE PARA ENCONTRAR EL CAMINO MAS CORTO ENTRE UN VERTICE Y TODOS LOS DEMAS
+
+
+}

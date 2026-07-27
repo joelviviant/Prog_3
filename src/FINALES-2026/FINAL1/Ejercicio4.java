00be9ff8f93189f9080e71d65ab9b1634ac534c8
jowi <joelviviant31@gmail.com>
2026-07-27 18:00:17 -0300

    ejercicio 4, a

---
+++ src/FINALES-2026/FINAL1/Ejercicio4.java
@@ -0,0 +1,18 @@
+public class Ejercicio4 {
+
+    //lista array ordenada
+    //Busqueda: O(log n), conozco el lugar del valor que quiero buscar
+    //Alta:agrego y corro una posicion todos los elementos
+    //Baja:elimino y corro una posicion todos los elementos
+
+    //Lista vinculada ordenada
+    //Busqueda: O(n), recorro la lista hasta encontrar el valor
+    //Alta: O(n), busco la posicion correcta e inserto, hago las nuevas vinculaciones
+    //Baja: O(n), busco el elemento y lo elimino, hago las nuevas vinculaciones
+
+    //Arbol binario de busqueda
+    //Busqueda: O(log n), recorro el arbol hasta encontrar el valor
+    //Alta: O(log n), busco la posicion correcta e inserto, hago las nuevas vinculaciones
+    //Baja: O(log n), busco el elemento y lo elimino, hago las nuevas vinculaciones
+
+}

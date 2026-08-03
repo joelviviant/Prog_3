4ca921f1ca73b8f4a6e8ccb1783d91b151c40564
jowi <joelviviant31@gmail.com>
2026-08-03 19:29:38 -0300

    EJERCICIO 5 TERMINADO

---
+++ src/FINALES-2026/FINAL2/Ejercicio5.java
@@ -0,0 +1,10 @@
+package FINAL2;
+
+public class Ejercicio5 {
+
+    //a. Falsa: En grafos dirigidos, un arco a un nodo visitado puede ser un cross-edge (arco cruzado) y no un ciclo. En grafos no dirigidos,
+    //si el nodo visitado es el padre directo, tampoco es un ciclo.
+    //b. Falsa: La complejidad de Quicksort es $O(n \log n) en el caso promedio/mejor, y $O(n^2) en el peor caso; nunca $O(n).
+    //c. Falsa: El problema del camino más largo es NP-hard y la estrategia greedy de Dijkstra falla al no poder manejar la reexploración de nodos o la presencia de ciclos de peso positivo.
+    //d. Falsa: La relación es inversa: un proceso contiene uno o más hilos (threads). Un hilo no puede contener procesos.
+}

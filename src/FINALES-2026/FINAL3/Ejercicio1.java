a165523b2aeb8ead72605d7e7ed6518d89be035c
jowi <joelviviant31@gmail.com>
2026-08-19 15:03:51 -0300

    EJERCICIO  1

---
+++ src/FINALES-2026/FINAL3/Ejercicio1.java
@@ -0,0 +1,31 @@
+package FINAL3;
+
+public class Ejercicio1 {
+
+   // Busqueda:
+   // 1- Arreglo Ordenado
+   //         Si el arreglo está ordenado, podés hacer búsqueda binaria → O(log n).
+    //         O(1) solamente sería si ya conocés la posición exacta del elemento.
+   // 2- Lista vinculada ordenada
+   //         O(n) - por mas que este ordenada si mi elemento esta ultimo debo recorrer tantas posiciones como elementos tenga
+   // 3- Arbol binario de busqueda
+   //        O(log n) promedio si está balanceado, O(n) en el peor caso.
+
+    //Alta:
+    // 1- Arreglo Ordenado
+    //         o(n) - debo desplazar todos los elementos siguientes para hacer espacio
+    // 2- Lista vinculada ordenada
+    //         o(n) - debo recorrer la lista hasta encontrar la posicion correcta
+    // 3- Arbol binario de busqueda
+    //         Promedio/balanceado: O(log n)
+    //         Peor caso: O(n)
+
+    //Baja:
+    //1- Arreglo Ordenado
+    //     o(n) - debo desplazar todos los elementos siguientes para llenar el espacio
+    //2- Lista vinculada ordenada
+    //     o(n) - debo recorrer la lista hasta encontrar la posicion correcta
+    //3- Arbol binario de busqueda
+    //     Promedio/balanceado: O(log n)
+    //     Peor caso: O(n)
+}
